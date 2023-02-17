7e176c108d8065dc45de5fe8c3fa8eccfa2d0407
yallala22 <114876017+yallala22@users.noreply.github.com>
2023-02-17 12:38:19 +0530

    third commit
    sssssss

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
 region = "us-east-1"
+  access_key = "AKIAVUQBNMPMJEWF2V5B"
+  secret_key = "EKLkjrFgAO0TkKn6b43MNBYAqB1AyXDwlb/J4Yp9"
 }
 resource "aws_instance" "example" {
 count = 2
